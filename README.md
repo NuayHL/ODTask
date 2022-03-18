c1f0657e9dfdfd36dfb2737e1df53301de4b554b
NuayHL <liuhaoyuan@akane.waseda.jp>
2022-03-18 09:54:30 +0800

    ubuntu first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Hard Work!
+### 17th Mar.
+Nothing special today... busy preparing the git but always met push failure..
+maybe I need focus on detection pipline
+### 18th Mar.
---
+++ util.py
@@ -0,0 +1,26 @@
+
+import matplotlib.pyplot as plt
+from torch import nn
+#小工具
+
+def printImg(img, title: str='', type = 0):
+    if type == 0: plt.imshow(img)
+    else: plt.imshow(img, cmap=plt.cm.gray)
+    plt.title(title)
+    plt.axis('off')
+    plt.show()
+
+def progressbar(percentage, endstr='', barlenth=20):
+    if int(percentage)==1: endstr +='\n'
+    print('\r[' + '>' * int(percentage * barlenth) +
+          '-' * (barlenth - int(percentage * barlenth)) + ']',
+          format(percentage * 100, '.1f'), '%', end=' '+endstr)
+
+def numofParameters(model: nn.Module ):
+    nump = 0
+    for par in model.parameters():
+        nump += par.numel()
+    return nump
+
+
+
