a982fd71060cb620e46866df39f444a0e8981eda
NauyHL <liuhaoyuan@akane.waseda.jp>
2022-03-21 22:14:16 +0800

    add training module

--- maindesk.py
+++ maindesk.py
@@ -1,7 +1,1 @@
-import torch
-from models.yolo import YOLOv3
 
-model = YOLOv3()
-test = torch.rand(10, 3, 256, 256)
-f1, f2 ,f3 = model(test)
-print(f1.shape, f2.shape, f3.shape)
---
+++ models/training.py
@@ -0,0 +1,7 @@
+import torch.nn.functional as F
+
+def yolo_loss(model_output, ground_truth):
+    pass
+
+def yolotraining(yolomodel, training_data, optimizer, loss=yolo_loss()):
+    pass
