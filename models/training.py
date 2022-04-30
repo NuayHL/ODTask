c22631282810dd264426bbfa2caa9cc68e22f1f2
NuayHL <liuhaoyuan@akane.waseda.jp>
2022-04-30 23:11:45 +0900

    pre assign code

--- models/training.py
+++ models/training.py
@@ -1,5 +1,11 @@
 import torch.nn.functional as F
 
+class Assign():
+    def __init__(self):
+        pass
+    def _retinaAssign(self,dt,gt):
+        pass
+
 def yolo_loss(model_output, ground_truth):
     pass
 
