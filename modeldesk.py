443434cf15d552b3d6725c26f2b9c4dd50ed1db8
NuayHL <liuhaoyuan@akane.waseda.jp>
2022-05-01 21:54:10 +0900

    pre iou code

--- modeldesk.py
+++ modeldesk.py
@@ -1,17 +1,10 @@
 #用于对模型进行测试
 
-from models.yolo import YOLOv3
+from models.training import Iou
 import torch
-import time
-from util.primary import numofParameters
+a = torch.Tensor([1,1,6,6]).cuda()
+b = torch.Tensor([2,2,6,6]).cuda()
 
-DEVICE = "cuda"
-
-a = torch.rand((3,3,512,512)).to(DEVICE)
-model = YOLOv3().to(DEVICE)
-
-print(numofParameters(model))
-tic = time.time()
-f1, f2, f3 = model(a)
-print(time.time()-tic)
-print(f1.shape, f2.shape, f3.shape)
+temp = Iou()
+iou = temp.make()
+print(iou(a,b))
--- models/training.py
+++ models/training.py
@@ -1,29 +1,34 @@
-import torch.nn.functional as F
+import torch
+
+ZEROTENSOR = torch.Tensor(0).cuda()
 
 class Iou():
-    def __init__(self,iou="iou",ip1="default",ip2="default"):
-        assert ip1 and ip2 in ["default","crowdhuman","diagonal"],"unknown format"
-        self.ip1 = ip1
-        self.ip2 = ip2
-        self.iou = iou
+    def __init__(self, iou="iou", ip1type="default", ip2type="default"):
+        assert ip1type and ip2type in ["default", "crowdhuman", "diagonal"], "unknown format"
+        self.ip1 = ip1type
+        self.ip2 = ip2type
+        self.type = iou
 
-    def _tranInput(self):
+    def _tranInput(self, ):
         pass
 
-def iou(a, b, type="default"):
-    assert len(a)==4 and len(b)==4,"bbox format unknown"
-    if type=="crowdhuman":
-        a[2], a[3] = a[0]+a[2], a[1]+a[3]
+    # need to compute for loss backward, how?
+    def iou(self, bbox1, bbox2):
+        xmin = max(bbox1[0],bbox2[0])
+        xmax = min(bbox1[2],bbox2[2])
+        ymin = max(bbox1[1],bbox2[1])
+        ymax = min(bbox1[3],bbox2[3])
+        xlen = max(ZEROTENSOR,xmax-xmin)
+        ylen = max(ZEROTENSOR,ymax-ymin)
+        join = xlen*ylen
+        to = (bbox1[2]-bbox1[0])*(bbox1[3]-bbox1[1])+(bbox2[2]-bbox2[0])*(bbox2[3]-bbox2[1])-join
+        return join/to
 
+    def make(self):
+        return self.iou
 
 class Assign():
     def __init__(self):
         pass
     def _retinaAssign(self,dt,gt):
         pass
-
-def yolo_loss(model_output, ground_truth):
-    pass
-
-def yolotraining(yolomodel, training_data, optimizer, loss=yolo_loss()):
-    pass
