351802266f55d5bb39e030c6d75b6ef752c8b847
NauyHL <liuhaoyuan@akane.waseda.jp>
2022-05-01 16:06:16 +0900

    pre iou

--- models/training.py
+++ models/training.py
@@ -1,5 +1,21 @@
 import torch.nn.functional as F
 
+class Iou():
+    def __init__(self,iou="iou",ip1="default",ip2="default"):
+        assert ip1 and ip2 in ["default","crowdhuman","diagonal"],"unknown format"
+        self.ip1 = ip1
+        self.ip2 = ip2
+        self.iou = iou
+
+    def _tranInput(self):
+        pass
+
+def iou(a, b, type="default"):
+    assert len(a)==4 and len(b)==4,"bbox format unknown"
+    if type=="crowdhuman":
+        a[2], a[3] = a[0]+a[2], a[1]+a[3]
+
+
 class Assign():
     def __init__(self):
         pass
