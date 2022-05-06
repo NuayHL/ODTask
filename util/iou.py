bc9dd3dcb29f9f2abeef8ee13a61ab199c4ba649
NuayHL <liuhaoyuan@akane.waseda.jp>
2022-05-06 23:40:50 +0900

    n

--- util/iou.py
+++ util/iou.py
@@ -3,10 +3,6 @@
 import torch
 import torch.nn as nn
 
-nn.Sigmoid()
-
-ZEROTENSOR = torch.Tensor([0]).cuda()
-
 class Iouloss(nn.Module):
     def __init__(self, ioutype="iou"):
         super(Iouloss, self).__init__()
@@ -28,9 +24,13 @@ class Iouloss(nn.Module):
         y_min = torch.max(dt_y1, gt_y1)
         y_max = torch.max(dt_y2, gt_y2)
 
-        w_int = x_max - x_min
-        h_int = y_max - y_min
+        w_int = torch.clamp(x_max - x_min,0)
+        h_int = torch.clamp(y_max - y_min,0)
 
+        join = w_int*h_int
+        union = (dt_x2-dt_x1)*(dt_y2-dt_y1)+(gt_x2-gt_x1)*(gt_y2-gt_y1)-join
+
+        return join/union
 
 class Iou():
     def __init__(self, ioutype="iou", ip1type="default", ip2type="default"):
@@ -49,8 +49,8 @@ class Iou():
         xmax = torch.min(bbox1[2:3],bbox2[2:3])
         ymin = torch.max(bbox1[1:2],bbox2[1:2])
         ymax = torch.min(bbox1[3:4],bbox2[3:4])
-        xlen = torch.max(ZEROTENSOR,xmax-xmin)
-        ylen = torch.max(ZEROTENSOR,ymax-ymin)
+        xlen = torch.clamp(xmax-xmin,0)
+        ylen = torch.clamp(ymax-ymin,0)
         join = xlen*ylen
         to = (bbox1[2:3]-bbox1[0:1])*(bbox1[3:4]-bbox1[1:2])+(bbox2[2:3]-bbox2[0:1])*(bbox2[3:4]-bbox2[1:2])-join
         return join/to
