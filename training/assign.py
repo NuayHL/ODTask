602ddfa52d052d17e8528837393b54618908cea4
NuayHL <liuhaoyuan@akane.waseda.jp>
2022-05-07 23:43:35 +0900

    yaml config

--- training/assign.py
+++ training/assign.py
@@ -1,12 +1,18 @@
 # this .py is for the assignment methods
 from .iou import IOU
+from .config import cfg
 
 class Assign():
-    def __init__(self,anchs,assignType="default",ioutype="iou"):
+    def __init__(self, _cfg = cfg):
+        if isinstance(_cfg, str):
+            from .config import Config
+            _cfg = Config(_cfg)
+            _cfg = _cfg()
         self.anchs = anchs
         self.lenAnchs = len(anchs)
         self.assignType = assignType
         self.iou = IOU(ioutype=ioutype)
+        self.threshold_iou = 0.5
     def assign(self,gt):
         if self.assignType == "default":
             return self._retinaAssign(gt)
@@ -16,7 +22,8 @@ class Assign():
             raise NotImplementedError("Unknown assignType")
 
     def _retinaAssign(self,gt):
-        lenGt = len(gt)
+        self.lenGt = len(gt)
+
         
 
     def _fcosAssign(self,gt):
--- training/config.py
+++ training/config.py
@@ -1,3 +1,15 @@
-# for training configs
+# generate cfg for training from yaml
+import yaml
 
+class Config():
+    def __init__(self,cfgPath):
+        with open(cfgPath,"r") as f:
+            self.cfg = yaml.safe_load(f.read())
+        self.assignType = self.cfg["assign_cfg"]["assignType"]
+        self.ioutype = self.cfg["assign_cfg"]["ioutype"]
+        self.threshold = self.cfg["assign_cfg"]["threshold"]
+    def __call__(self):
+        return self.cfg
 
+config = Config("config.yaml")
+cfg = config()
