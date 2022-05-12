8991355d15d4bc5a9a415bb3cc55cb11b24dfd72
NauyHL <liuhaoyuan@akane.waseda.jp>
2022-05-13 08:59:06 +0900

    add transforms part

--- maindesk.py
+++ maindesk.py
@@ -15,6 +15,8 @@ from torch.utils.data import DataLoader
 import numpy as np
 import torch
 from models.yolo import Yolov3_core
+from models.backbone import Darknet53
+from models.darknet53 import darknet53
 from training.assign import AnchAssign
 from training.config import cfg
 from util.primary import numofParameters
@@ -25,15 +27,21 @@ testinput = torch.rand((8,3,512,512))
 
 model = Yolov3_core(numofclasses=1).cuda()
 print(numofParameters(model))
-dataset = CrowdHDataset("CrowdHuman/annotation_train_coco_style.json")
-loader = DataLoader(dataset, batch_size=cfg.batch_size, collate_fn=OD_default_collater)
-for batch in loader:
-    print(batch["imgs"].shape)
-    input = batch["imgs"].cuda()
-    result = model(input)
-    break
 
-print(result[0].shape,result[1].shape,result[2].shape)
+model1 = Darknet53(numofclasses=1).cuda()
+print(numofParameters(model1))
+
+model2 = darknet53(1).cuda()
+print(numofParameters(model2))
+# dataset = CrowdHDataset("CrowdHuman/annotation_train_coco_style.json")
+# loader = DataLoader(dataset, batch_size=cfg.batch_size, collate_fn=OD_default_collater)
+# for batch in loader:
+#     print(batch["imgs"].shape)
+#     input = batch["imgs"].cuda()
+#     result = model(input)
+#     break
+
+# print(result[0].shape,result[1].shape,result[2].shape)
 
 
 
