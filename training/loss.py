91b2b993a6fae3cbc91ba929ef4f82c77dea9d63
NuayHL <liuhaoyuan@akane.waseda.jp>
2022-05-13 10:51:06 +0900

    begin loss function

--- training/loss.py
+++ training/loss.py
@@ -1,8 +1,13 @@
 import torch
 import torch.nn as nn
 from training.config import cfg
+from training.assign import AnchAssign
+from models.anchor import generateAnchors
 
 class Defaultloss(nn.Module):
+    def __init__(self,):
+        super(Defaultloss, self).__init__()
     def forward(self,dt,an,gt):
         pass
+
 
