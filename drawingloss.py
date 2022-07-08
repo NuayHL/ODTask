65c7a148d7c16c7dfd36999b1b04c082e7cfc825
NauyHL <liuhaoyuan@akane.waseda.jp>
2022-07-08 13:25:19 +0900

    adding segmentation to the annotation

--- drawingloss.py
+++ drawingloss.py
@@ -1,5 +1,5 @@
 from util.visualization import draw_loss, draw_loss_epoch, draw_coco_eval
 
-draw_loss("100E_8B_800_1024_Retinanet2nd_test.txt")
-draw_loss_epoch("100E_8B_800_1024_Retinanet2nd_test.txt",50)
-#draw_coco_eval("100E_16B_608_608_Darknet53NoFocal_widerperson_eval.txt")
+draw_loss("120E_8B_608_608_yolo_resnet18_test.txt")
+draw_loss_epoch("120E_8B_608_608_yolo_resnet18_test.txt",1875)
+draw_coco_eval("120E_8B_608_608_yolo_resnet18_test_eval.txt")
